977f57ac5ad032b61ed20a3ed5539a26b424c923
ramdess <emmanuelramde4@gmail.com>
2025-09-11 15:08:46 +0000

    modi

--- maths/settings.py
+++ maths/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = "django-insecure-zk9&^*qhru8zer%aqi0r$$x3fw6+21#1g-=)n@g6%nca4@s)30
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ramdess.pythonanywhere.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
-
+### chemin:ramdess.pythonanywhere.com','127.0.0.1'
 # Application definition
 
 INSTALLED_APPS = [
